4951351a2e5d7285def278583ef2851ac81a8713
gracietsanchez <78111898+gracietsanchez@users.noreply.github.com>
2022-02-09 08:20:47 -0800

    Lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary (use period)
+// Description (use period)
+// @ Link URL
+// @file defines the MyClass Class.
+// @author AuthorName.
+//@since x.x.x
+
+  //Program asks user for name then greets user with inputted naem
+    //Ask User For name
+      //Print Text "Enter Your Name"
+      //Allow user input
+    //Accept User input
+    //Validate User input
+    //Print greeting with user input included
